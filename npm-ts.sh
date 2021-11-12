7eb5aa729d5b2b02acb0c881477e967f0778159d
neoncitylights <sam.t.nguyenn@gmail.com>
2021-11-12 10:54:52 -0600

    Create npm-ts.sh

---
+++ npm-ts.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npm install @types/jest @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-plugin-jest gh-pages ts-jest ts-loader typedoc typescript webpack webpack-cli
