56b62ae2abac0666b4af66e3ccbc5e282ba74b83
Samantha Nguyen <contact@samanthanguyen.me>
2024-05-15 00:49:58 -0500

    cleanup

--- scripts/npm-ts.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-npm install @types/jest @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-plugin-jest gh-pages ts-jest ts-loader typedoc typescript webpack webpack-cli
