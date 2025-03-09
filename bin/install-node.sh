3987d798dfac90a2c32f09d05e82ecbcaad273be
Samantha Nguyen <contact@samanthanguyen.me>
2025-03-09 00:32:43 -0600

    actually include install scripts lol

---
+++ bin/install-node.sh
@@ -0,0 +1,12 @@
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 23
+
+# Verify the Node.js version:
+node -v # Should print "v23.9.0".
+nvm current # Should print "v23.9.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
