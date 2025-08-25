2abd15a985d440a81ec542076db40b6ae63f9835
Samantha Nguyen <contact@samanthanguyen.me>
2025-08-25 15:41:16 -0500

    don't hardcode versions in comments where possible

--- bin/install-nvm-node.sh
+++ bin/install-nvm-node.sh
@@ -9,9 +9,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 nvm install node --latest-npm
 nvm use node
 
-# Verify the Node.js version:
-node -v # Should print "v23.9.0".
-nvm current # Should print "v23.9.0".
+# Verify the Node.js version, both of these should print the same
+node -v
+nvm current
 
-# Verify npm version:
-npm -v # Should print "10.9.2".
+# Verify npm version
+npm -v
